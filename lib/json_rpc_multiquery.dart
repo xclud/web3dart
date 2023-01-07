c1f0af96e575fc0ce453905ca0fbc71d758a74e2
Juampi Q <juampiq6@gmail.com>
2023-01-07 13:46:01 +1100

    documented multiqueryRpcService interface instead of throwing an RPCError, now an error is returned in the list of responses, to be handled by the receiver appropiately

--- lib/json_rpc_multiquery.dart
+++ lib/json_rpc_multiquery.dart
@@ -1,15 +1,35 @@
+library json_rpc_multiquery;
+
 import 'dart:convert';
 
 import 'package:http/http.dart';
 
+import 'contracts.dart';
+import 'credentials.dart';
+import 'crypto.dart';
 import 'json_rpc.dart';
+import 'src/core/block_number.dart';
 
-class JsonRPCMultiQuery extends JsonRPC {
+abstract class MultiQueryRpcService {
+  /// Performs a single RPC request, asking the server to execute several queries
+  /// using the functions with associated parameters for each one, the parameters
+  /// need to be encodable with the [json] class of dart:convert.
+  ///
+  /// When the request is successful, a list is returned, containing each query
+  /// response. This responses can be either an [RPCResponse] on success, or an
+  /// [RPCError] on failure.
+  /// No [RPCError] instances will be thrown, they will only be part of the list.
+  /// Other errors might be thrown if an IO-Error occurs.
+  Future<List<dynamic>> callMultiQuery(List<RPCQuery> queries);
+}
+
+class JsonRPCMultiQuery extends JsonRPC implements MultiQueryRpcService {
   JsonRPCMultiQuery(String url, Client client) : super(url, client);
 
   int _currentRequestId = 1;
 
-  Future<List<RPCResponse>> callMultiQuery(List<RPCQuery> queries) async {
+  @override
+  Future<List<dynamic>> callMultiQuery(List<RPCQuery> queries) async {
     final payloadList = <Map<String, dynamic>>[];
     for (final query in queries) {
       payloadList.add(
@@ -27,20 +47,22 @@ class JsonRPCMultiQuery extends JsonRPC {
       body: json.encode(payloadList),
     );
 
-    final responses = <RPCResponse>[];
+    // responses list will be RPCResponse and/or RPCError instances
+    final responses = <dynamic>[];
 
     final dataList = json.decode(response.body) as List<dynamic>;
     final castedList = dataList.cast<Map<String, dynamic>>();
 
     for (final data in castedList) {
       if (data.containsKey('error')) {
+        final id = data['id'] as int;
         final error = data['error'];
 
         final code = error['code'] as int;
         final message = error['message'] as String;
         final errorData = error['data'];
 
-        throw RPCError(code, message, errorData);
+        responses.add(RPCError(code, message, errorData, id));
       }
 
       final id = data['id'] as int;
@@ -52,8 +74,43 @@ class JsonRPCMultiQuery extends JsonRPC {
 }
 
 class RPCQuery {
-  RPCQuery(this.function, [this.params]);
+  RPCQuery(this.function, [this.params, this.id]);
 
   final String function;
+  final String? id;
   final List<dynamic>? params;
+}
+
+/// Useful class to
+class EthContractCall {
+  EthContractCall({
+    this.sender,
+    required this.contract,
+    required this.function,
+    required this.params,
+    this.atBlock = const BlockNum.current(),
+  });
+
+  final EthereumAddress? sender;
+  final DeployedContract contract;
+  final ContractFunction function;
+  final List<dynamic> params;
+  final BlockNum? atBlock;
+
+  RPCQuery toRPCQuery(String id) {
+    final data = function.encodeCall(params);
+    final rawParamsBody = {
+      'to': contract.address.hex,
+      'data': bytesToHex(data, include0x: true, padToEvenLength: true),
+      if (sender != null) 'from': sender!.hex,
+    };
+    return RPCQuery(
+      'eth_call',
+      [
+        rawParamsBody,
+        atBlock,
+      ],
+      id,
+    );
+  }
 }
