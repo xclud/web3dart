20f37cae8d7ff45b10abb2c8ecd0647ef39d8bb0
developerpaaji <bhavneet.dev@gmail.com>
2023-03-08 19:04:14 +0530

    remove extra line

--- lib/web3dart.dart
+++ lib/web3dart.dart
@@ -27,7 +27,6 @@ export 'credentials.dart';
 export 'src/core/block_number.dart';
 export 'src/core/sync_information.dart';
 
-
 part 'src/core/ether_unit.dart';
 part 'src/core/ether_amount.dart';
 part 'src/core/block_information.dart';
