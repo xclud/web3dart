7405bd6f97862749171e24f7224d3e36a76f77d3
kirill09 <kirill_belyshev@mail.ru>
2023-06-12 14:29:22 +0300

    calculation of max fee per gas (#108)

--- lib/src/core/transaction.dart
+++ lib/src/core/transaction.dart
@@ -94,5 +94,5 @@ class Transaction {
     );
   }
 
-  bool get isEIP1559 => maxFeePerGas != null && maxPriorityFeePerGas != null;
+  bool get isEIP1559 => maxFeePerGas != null || maxPriorityFeePerGas != null;
 }
